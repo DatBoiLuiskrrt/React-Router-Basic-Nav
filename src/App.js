6b85cfe60e7f1b8ac9ed71d007f25ef08fd01177
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-02-19 16:16:06 -0600

    met most viable product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {Router, Route} from 'react-router-dom';
 
 const App = () => (
-  <div>
+  <>
+ 
     <Navigation />
-  </div>
+ 
+  <Route exact path="/">
+    <Home/>
+  </Route>
+  <Route exact path="/about">
+    <About/>
+  </Route>
+  <Route exact path="/contact">
+    <Contact/>
+  </Route>
+  </>
 );
 
 export default App;
